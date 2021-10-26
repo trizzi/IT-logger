dea03f6a7948b1a41c800af3b3319b6095d8ac30
Ric <ric@Doom-Machine.local>
2021-10-26 19:05:33 +0100

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	useEffect(() => {
+		// initializes materialize js
+		M.AutoInit();
+	});
+	return (
+		<div className='App'>
+			<h1>Hello Motherfucker!!!!</h1>
+		</div>
+	);
+};
 
 export default App;
